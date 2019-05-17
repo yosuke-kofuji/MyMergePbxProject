d7c8096eb7b4c4253eebac5f27e5599f0c216a85
yosuke-kofuji <ykofuji@sonix.asia>
2019-05-17 16:29:14 +0900

    Initial Commit

---
+++ MyMergePbxProject/Hoge.swift
@@ -0,0 +1,13 @@
+//
+//  Hoge.swift
+//  MyMergePbxProject
+//
+//  Created by ykofuji on 2019/05/17.
+//  Copyright © 2019 ykofuji. All rights reserved.
+//
+
+import Foundation
+
+class Hoge {
+
+}
